b021558c11226a4df26e2ec5ddb5f6d38d5e4179
Dostonkhuja <sheraliyedostonhoja@gmail.com>
2026-03-19 22:06:31 +0500

    vite config and script fixed to build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personal-web-site/',
+  base: '/personal-web-site/', // GitHub Pages repo nomi
   plugins: [react()],
-})
+});
